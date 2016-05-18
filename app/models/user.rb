4795283800cd8862e1ec0100049595907bd9363b
Alhuar <alfonsohuescar@gmail.com>
2016-05-18 12:42:04 +0200

    Fix user relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 has_many :bands, :through => :users_band
 has_many :users_band
+#validates :email, uniqueness: true
+before_save { self.email = email.downcase }
+
+validates :name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
