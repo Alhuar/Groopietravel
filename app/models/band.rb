37575f0ef9c16c8366e81ef24ccb70b678a07200
Alhuar <alfonsohuescar@gmail.com>
2016-05-18 12:19:52 +0200

    Fixed band-concert relation

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,6 +1,7 @@
 class Band < ActiveRecord::Base
 	has_many :users, :through => :users_band
 	has_many :users_band
+	has_many :concerts
 	validates :name, presence: true	
 	validates :image, presence: true
 	validates :description, presence: true
