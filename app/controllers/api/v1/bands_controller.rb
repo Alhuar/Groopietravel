5689b880c2c3010594864bfbc1c27b57734b69a0
Alhuar <alfonsohuescar@gmail.com>
2016-05-23 13:11:28 +0200

    Add check if query exists method to api band controller

--- app/controllers/api/v1/bands_controller.rb
+++ app/controllers/api/v1/bands_controller.rb
@@ -13,19 +13,49 @@ class Api::V1::BandsController < ApplicationController
 	end
 
 	def search
-		bandName = params[:band].capitalize
-		band = Band.find_by(name: bandName)
-		#binding.pry
-		unless band
-			render json: {band: bandName, error: "not found"}
+		bandname = params[:band].downcase.titleize
+		@band = Band.find_by(name: bandname)
+		unless @band
+			render json: {band: bandname, error: "not found"}
 			return
 		end
 		
-		# redirect_to user
-		# render json: @band 
-		# return
-
-		#render json: band
+		render json: {success: true, band: @band}
+		#redirect_to users_path 
 	end
 
+	def create
+		band = Band.create(
+			name: params[:artistName],
+			on_tour_until: params[:onTourUntil].to_date.strftime("%a %d %b %Y")
+		)
+		params['events'].each do |key, value|
+			date = value["date"].to_date.strftime("%a %d %b %Y")
+			city = value["city"]
+			latitude = value["lat"]
+			longitude = value["lng"]
+			name = value["name"]
+			venue = value["venue"]
+			band.concerts.create(
+				date: date, 
+				city: city, 
+				name: name, 
+				venue: venue, 
+				longitude: longitude, 
+				latitude: latitude
+				)
+		end
+		current_user.bands.push(band)
+		render json: band
+	end
+
+	def favorite_band
+    band = Band.find(params[:id])
+    @query = UsersBand.find_by(user: current_user, band: band)
+    Band.check_if_query_exists?(query, current_user, band)
+    # query = UsersBand.find_by(user_id: current_user.id, band_id: band.id)
+    #Y con esto: =>
+    query.update_attribute(:is_favorite, true) unless query.is_favorite
+	  render json: band
+	end
 end
