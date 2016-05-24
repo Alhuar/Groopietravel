b439983cf1421da3db6e3f3e2753382f95a9791a
Alhuar <alfonsohuescar@gmail.com>
2016-05-24 18:38:23 +0200

    add geokit to Bands controller

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,12 +1,20 @@
 class BandsController < ApplicationController
+	include Geokit::Geocoders
+
 	def index
 		user = User.find(params[:user_id])
 		#@user_bands = Band.where("user_id = ?", @user.id)
-		@bands = current_user.bands
+		@user_bands = user.bands
 	end
 	
 	def show
 		@band = current_user.bands.find(params[:id])
 		@concerts = @band.concerts
+		a = Geokit::LatLng.new(@band.concerts[0].latitude,@band.concerts[0].longitude)
+		b = Geokit::LatLng.new(@band.concerts[1].latitude,@band.concerts[1].longitude)
+		@distance = a.distance_to(b)
+		@city1 = @band.concerts[0].city
+		@city2 = @band.concerts[1].city
+		#@all_concerts = current_user.concerts_count
 	end
 end
