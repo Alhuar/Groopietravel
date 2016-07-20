c21bb3518f4b5bc22b8119bf80f29d71e050e0bf
Alhuar <alfonsohuescar@gmail.com>
2016-07-20 10:40:37 +0200

    move band api controller logic to band model

--- app/controllers/api/v1/bands_controller.rb
+++ app/controllers/api/v1/bands_controller.rb
@@ -5,7 +5,6 @@ class Api::V1::BandsController < ApplicationController
 	
 	def show
 		@band = current_user.bands.find(params[:id])
-		@concerts = @band.concerts
 	end
 
 	def search
@@ -23,44 +22,23 @@ class Api::V1::BandsController < ApplicationController
 			name: params[:artistName],
 			on_tour_until: params[:onTourUntil].to_date.strftime("%a %d %b %Y")
 		)
-		
-		filtered_events = {}
-		
-		params['events'].each do |item|
-		  
-		  if filtered_events.has_key? item[1][:date]
-		    filtered_events[item[1][:date]].merge! item[1]
-		  else
-		    filtered_events[item[1][:date]] = item[1]
-		  end
-		end
-		my_array = filtered_events.collect { |key, value| value }
-		 # binding.pry
-		my_array.each do |item|
-			date = item["date"].to_date.strftime("%a %d %b %Y")
-			city = item["city"]
-			latitude = item["lat"].to_f
-			longitude = item["lng"].to_f
-			name = item["name"]
-			venue = item["venue"]
-			band.concerts.create(
-				date: date, 
-				city: city, 
-				name: name, 
-				venue: venue, 
-				longitude: longitude, 
-				latitude: latitude
-				)
-		end
-
+		band.save_concerts(params['events'])
 		current_user.bands.push(band)
 		render json: band
 	end
 
 	def favorite_band
-    band = Band.find(params[:id])
-    query = UsersBand.find_by(user: current_user, band: band) || Band.check_if_query_exists?(query, current_user, band)
-    Band.make_favorite(query)
-	  render json: band
+    	band = Band.find(params[:id])
+    	query = UsersBand.find_by(user: current_user, band: band) || Band.check_if_query_exists?(query, current_user, band)
+    	Band.make_favorite(query)
+	 	 render json: band
+	end
+
+	def unfavorite
+    	band = Band.find(params[:id])
+		query = UsersBand.find_by(user: current_user, band: band)
+		Band.make_unfavorite(query)
+		#query.update_attribute(:is_favorite, false)
+		render json: band
 	end
 end
--- app/models/band.rb
+++ app/models/band.rb
@@ -3,23 +3,53 @@ class Band < ActiveRecord::Base
 	has_many :users_band
 	has_many :concerts
 	validates :name, presence: true	
-	# validates :image, presence: true
+	validates :on_tour_until, presence: true
 	# validates :description, presence: true
-
-
-	# def make_favorite(user)
-	# 	query = UsersBand.find_by(user: user.id, band: band.id)
-	# 	query.update_attribute(:is_favorite, true) unless query.is_favorite		
-	# end
-
-
 
 	def self.check_if_query_exists?(query, user, band)
 		unless query.present?
 			user.bands.push(band)
 		end
-		@query = UsersBand.find_by(user_id: user.id, band_id: band.id)
+		UsersBand.find_by(user_id: user.id, band_id: band.id)
 	end
+
+	def self.make_favorite(query)
+		query.update_attribute(:is_favorite, true) unless query.is_favorite
+	end
+
+	def self.make_unfavorite(query)
+		query.update_attribute(:is_favorite, false) unless !query.is_favorite
+	end
+
+	def save_concerts(events)
+		filtered_events = {}
+		events.each do |item|  
+		  if filtered_events.has_key? item[1][:date]
+		    filtered_events[item[1][:date]].merge! item[1]
+		  else
+		    filtered_events[item[1][:date]] = item[1]
+		  end
+		end
+		my_array = filtered_events.collect { |key, value| value }
+		my_array.each do |item|
+			date = item["date"].to_date.strftime("%a %d %b %Y")
+			city = item["city"]
+			latitude = item["lat"].to_f
+			longitude = item["lng"].to_f
+			name = item["name"]
+			venue = item["venue"]
+			self.concerts.create(
+				date: date, 
+				city: city, 
+				name: name, 
+				venue: venue, 
+				longitude: longitude, 
+				latitude: latitude
+			)
+		end
+
+	end
+
 end
 
 
